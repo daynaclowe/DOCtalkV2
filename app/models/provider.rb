8d651396d8d2d95b17b9a67e69b4bff0704b634b
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-01-12 23:09:02 -0500

    Added validations and relationship between provider and ratings

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,8 @@
 class Provider < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :organization_name, :kind, :address_line1, :postal_code, :city, :province, :phone, :waiting_period
+
   has_many :reviews
+  has_many :ratings
 
   def full_address
      "#{address_line1}\n#{address_line2}\n#{city}, #{province}, #{postal_code}"
