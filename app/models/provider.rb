5037fdb71983b656886805bdf251b999a791e824
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-01-13 15:09:50 -0500

    Added new relationships, as well as new mthods

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,10 +1,25 @@
 class Provider < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :organization_name, :kind, :address_line1, :postal_code, :city, :province, :phone, :waiting_period
 
+  belongs_to :user
+  
   has_many :reviews
   has_many :ratings
+
+  has_many :users_that_rated_this, through: :ratings, source: :user
+  has_many :users_that_reviewed_this, through: :reviews, source: :user
 
   def full_address
      "#{address_line1}\n#{address_line2}\n#{city}, #{province}, #{postal_code}"
   end
+
+  def ratings_by_user(user)
+    rating = ratings.find_by(user: user)
+    rating.average_rating_by_user
+  end
+
+  def overall_rating
+    (self.ratings.sum("knowledge + support + comfort + accessibility + recommendation") / (20 * ratings.count(:user_id).to_f) * 100).round(0)
+  end
+
 end
