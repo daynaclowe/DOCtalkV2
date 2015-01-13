03ffb9b3312e6658a467483db8cd686a36602824
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-01-13 15:11:09 -0500

    Added relationships between models and methods to confirm ratings on provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  has_many :reviews
+  has_many :ratings
+  has_many :providers
+  has_many :providers_reviewed, through: :reviews, source: :providers
+  has_many :providers_rated, through: :ratings, source: :providers
+
+  def full_name
+    "{first_name} #{last_name}"
+  end
+
+  def rated?(provider)
+    ratings.find_by(provider: provider)
+  end
+
+
 end
