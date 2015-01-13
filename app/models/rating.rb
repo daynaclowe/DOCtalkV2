b430700446f6cf0e9bdee8b1d67249e4fd91925f
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-01-12 23:10:10 -0500

    Added validations as well as belongs_to relationships, and method to determine an average rating by a single user.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,11 @@
 class Rating < ActiveRecord::Base
+  validate :knowledge, :support, :comfort, :accessibility, :recommendation
+
+  belongs_to :user
+  belongs_to :provider
+
+  def average_rating_by_user
+    ((knowledge + support + comfort + accessibility + recommendation ) /20.0) * 100
+  end
+
 end
