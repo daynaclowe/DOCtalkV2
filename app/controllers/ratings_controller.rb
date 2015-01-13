14f85217d635c0ee4ecb505293de382b6fed43a1
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-01-12 23:18:21 -0500

    Added basic crud methods and private methods that carry over the previous provider ratings

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,53 @@
+class RatingsController < ApplicationController
+before_filter :load_provider
+before_filter :authenticate_user!, :except => [:show]
+before_filter :load_rating, only:[:show, :destroy, :edit, :update]
+
+  def show
+  end
+
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    @rating = @provider.ratings.build(rating_params)
+      if current_user
+        @rating.user = current_user
+      end
+
+      if @rating.save
+        redirect_to @provider, notice: 'Rating successfully created'
+      else
+        render 'providers/show'
+      end
+  end
+
+  def edit 
+    @rating = Rating.find(params[:id])
+  end
+
+  def update
+    if @rating.update(rating_params)
+      redirect_to @provider
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:knowledge, :support, :comfort, :accessibility, :reccomendation, :provider_id, :user_id, :kind)
+  end
+
+  def load_provider
+    
+    @provider = Provider.find(params[:provider_id])
+  end
+
+  def load_rating
+    @rating = Rating.find(params[:id])
+  end
+
+end
