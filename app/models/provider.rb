0bf02d1d66e479cae9c99ee8c1d168e67a1f3c61
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-01-17 16:29:02 -0500

    Added mount_uploader for carrierwave adn has_many relationship to orgainzations

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,10 @@
 class Provider < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :kind, :address_line1, :postal_code, :city, :province, :phone, :waiting_period
 
+  mount_uploader :image, ImageUploader
+  
   belongs_to :user
+  has_many :organizations, through: :relationships
   
   has_many :reviews
   has_many :ratings
@@ -15,10 +18,10 @@ class Provider < ActiveRecord::Base
      "#{address_line1}\n#{address_line2}\n#{city}, #{province}, #{postal_code}"
   end
 
-  def ratings_by_user(user)
-    rating = ratings.find_by(user: user)
-    rating.average_rating_by_user
-  end
+  # def ratings_by_user(user)
+  #   rating = ratings.find_by(user: user)
+  #   rating.average_rating_by_user
+  # end
 
   def overall_rating
     (self.ratings.sum("knowledge + support + comfort + accessibility + recommendation") / (20 * ratings.count(:user_id).to_f) * 100).round(0)
