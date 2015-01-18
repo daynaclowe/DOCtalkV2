a9bbe521ee2635eec892d2256847bc42ae6d5fd0
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-01-18 13:59:08 -0500

    repetative column username removed, have user_name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115195930) do
+ActiveRecord::Schema.define(version: 20150118175912) do
 
   create_table "contacts", force: true do |t|
     t.string   "first_name"
