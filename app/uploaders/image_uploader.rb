48f4260ee1ee298be542fffc757fba331063e215
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-01-18 14:02:27 -0500

    Folder for fallback image ie if a user doesnt upload an image for a provider or organization this should fill that space - unfinished

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,7 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :avatar do
-    process :resize_to_fit => [200, 200]
+    process :resize_to_fit => [400, 400]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
