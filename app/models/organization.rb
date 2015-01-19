2e49f1fdcf0ab636788ad18e4921cbe3849d304f
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-01-18 20:00:56 -0500

    Added a search method to Organization#model that checks to see which options in our search form have been selected and searches based on those selected parameters

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,4 +5,17 @@ class Organization < ActiveRecord::Base
   has_many :reviews
   has_many :ratings
 
+  def self.search(param_name, param_city)
+    results = Organization.all
+    if param_name.present?
+      results = results.where("name LIKE ?", "%#{param_name}%")
+    end
+    if param_city.present?
+      if param_city == "Any"
+      else
+        results = results.where(city: param_city)
+      end
+    end
+    results
+  end
 end
